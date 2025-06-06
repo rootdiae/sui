8624733932821936b0774bb7a5790de4c73e4569
rootdiae <rootdiae@users.noreply.github.com>
2025-06-06 01:43:59 +0000

    deploy: 144272723ddbb6cca6bb27bffa218ab0e58c6de6

---
+++ sui_adapter_latest/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"macro":["sp"],"mod":["adapter","error","execution_engine","execution_mode","execution_value","gas_charger","gas_meter","programmable_transactions","static_programmable_transactions","temporary_store","type_layout_resolver","type_resolver"]};
